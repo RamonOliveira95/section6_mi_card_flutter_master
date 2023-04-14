9b8b3c763eb13935b49a6fed34b824976b8613f9
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2023-04-14 11:56:42 -0300

    1.0.2

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 - Após isso deixei tudo em uma Column, pois o formato do app é na vertical;
 - Adicionado imagem referente ao perfil do usuário e o nome, logo a baixo.
 
-# AULA 2 1.0.1
+# AULA 2 - 1.0.1
 - Adicionado novas fontes ao projeto, pegando as fontes no site fonts.google.com ;
 - Adicionado profissão do usuário logo abaixo do nome.
+
+# AULA 3 - 1.0.2
+- Adicionar duas linhas abaixo da profissão, onde uma mostre o telefone de contato e a outra mostre o e-mail;
+- Na esquerda de cada uma dessas linhas, antes do telefone e do e-mail tem que conter uma ícone que se refiram a essas palavras.
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
-          children:  [
+          children: [
             const CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('images/perfil.jpg'),
@@ -37,6 +37,48 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: const EdgeInsets.all(10),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  const SizedBox(width: 20),
+                  Text(
+                    '+55 51 123.456.789',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: const EdgeInsets.all(10),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.email_outlined,
+                    color: Colors.teal,
+                  ),
+                  const SizedBox(width: 20),
+                  Text(
+                    'homer.simpson@nuclear.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20),
+                  ),
+                ],
+              ),
+            )
           ],
         )),
       ),
